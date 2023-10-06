e4f2f94eea8828d8ea2f90ab1ad3d9b4a07b0490
Marcelo Brito <marcellus.brito@gmail.com>
2023-10-05 23:29:40 -0300

    new favico and data  componenent

---
+++ src/Components/Data.js
@@ -0,0 +1,38 @@
+export const petData = [
+  {
+    id: "1",
+    name: "My Life Pet Clinic",
+    latitude: -23.449398559296387,
+    longitude: -46.53919433578667,
+  },
+  {
+    id: "2",
+    name: "Pet Shop Pink Dog",
+    latitude: -23.445224533703712,
+    longitude: -46.54664986976165,
+  },
+  {
+    id: "3",
+    name: "Pet Shop Smax",
+    latitude: -23.438271368446387,
+    longitude: -46.5557591750829,
+  },
+  {
+    id: "4",
+    name: "Darling Pet shop",
+    latitude: -23.452314773911944,
+    longitude: -46.510170711865904,
+  },
+  {
+    id: "5",
+    name: "Pet Shop Feed The Consulate",
+    latitude: -23.46687944828309,
+    longitude: -46.589202111359164,
+  },
+  {
+    id: "6",
+    name: "Studio Pet Store Spa Club",
+    latitude: -23.463061295703046,
+    longitude: -46.60027391042241,
+  },
+];
--- src/Components/Map.js
+++ src/Components/Map.js
@@ -7,6 +7,7 @@ import RestartAltIcon from "@mui/icons-material/RestartAlt";
 import GoogleMapReact from "google-map-react";
 import MyLocationIcon from "@mui/icons-material/MyLocation";
 import PetsIcon from "@mui/icons-material/Pets";
+import { petData } from "./Data";
 
 export default class Map extends React.Component {
   constructor(props) {
@@ -222,42 +223,3 @@ export default class Map extends React.Component {
     );
   }
 }
-
-const petData = [
-  {
-    id: "1",
-    name: "My Life Pet Clinic",
-    latitude: -23.449398559296387,
-    longitude: -46.53919433578667,
-  },
-  {
-    id: "2",
-    name: "Pet Shop Pink Dog",
-    latitude: -23.445224533703712,
-    longitude: -46.54664986976165,
-  },
-  {
-    id: "3",
-    name: "Pet Shop Smax",
-    latitude: -23.438271368446387,
-    longitude: -46.5557591750829,
-  },
-  {
-    id: "4",
-    name: "Darling Pet shop",
-    latitude: -23.452314773911944,
-    longitude: -46.510170711865904,
-  },
-  {
-    id: "5",
-    name: "Pet Shop Feed The Consulate",
-    latitude: -23.46687944828309,
-    longitude: -46.589202111359164,
-  },
-  {
-    id: "6",
-    name: "Studio Pet Store Spa Club",
-    latitude: -23.463061295703046,
-    longitude: -46.60027391042241,
-  },
-];
